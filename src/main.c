9982f968973d54f9d790b3a1f6564cfbc35ae2f1
root <root@localhost>
2024-10-04 07:29:50 +0300

    auto publish 1728016190638

---
+++ src/main.c
@@ -0,0 +1,13 @@
+// calculation of compound interest
+#include <stdio.h>
+int main() {
+//declaration and intiaisation
+double principal = 20000;
+double rate =4.5 // 4.5% interest rate
+double time = 4; // 4 years
+
+double amount = principal*((pow((1+rate/n),n*time)));
+printf("the amount(double) is %f",amount);
+return 0;
+}
+	
